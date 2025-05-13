aaded42b1c6c50bc91c292f12eefe574ff87e891
Chengshu Li <chengshu@stanford.edu>
2025-05-13 16:28:19 -0700

    further improve plot stats.py

--- mimicgen/scripts/plot_stats.py
+++ mimicgen/scripts/plot_stats.py
@@ -35,24 +35,34 @@ ABLATIONS = ["full"]
 
 for ABLATION in ABLATIONS:
     FOLDER = f"{TASK}_{ABLATION}"
-    if TASK == "tidy_table" and ABLATION == "full":
-        PATH = f"{ROOT_DIR}/{SRC_USER}/{PROJECT}/{FOLDER}_old"
-    else:
-        PATH = f"{ROOT_DIR}/{SRC_USER}/{PROJECT}/{FOLDER}"
+    PATH = f"{ROOT_DIR}/{SRC_USER}/{PROJECT}/{FOLDER}"
+    DST_PATH = f"{ROOT_DIR}/{DST_USER}/{PROJECT}/{FOLDER}"
     for folder in sorted(os.listdir(PATH)):
         print(ABLATION, folder)
         folder_path = os.path.join(PATH, folder)
+        dst_folder_path = os.path.join(DST_PATH)
         for subfolder in sorted(os.listdir(folder_path)):
             if DR not in subfolder: continue
+            important_stats_json_file = os.path.join(folder_path, subfolder, "important_stats.json")
             stats_json_file = os.path.join(folder_path, subfolder, "subtask_lengths.json")
+            dst_important_stats_json_file = os.path.join(dst_folder_path, subfolder, "important_stats.json")
+            dst_stats_json_file = os.path.join(dst_folder_path, subfolder, "subtask_lengths.json")
+            dst_subfolder = os.path.join(dst_folder_path, subfolder)
+            if not os.path.isdir(dst_subfolder):
+                os.makedirs(dst_subfolder)
             demo_hdf5_file = os.path.join(folder_path, subfolder, "demo.hdf5")
-            if os.path.isfile(stats_json_file): continue
-            subtask_lengths = dict()
-            if not os.path.isfile(demo_hdf5_file): continue
-            print(demo_hdf5_file)
-            with h5py.File(demo_hdf5_file, "r") as hdf5_f:
-                for demo_key in hdf5_f["data"]:
-                    subtask_lengths[demo_key] = hdf5_f["data"][demo_key]["subtask_lengths"][:].tolist()
-            print("num of demos stats collected", len(subtask_lengths))
-            with open(stats_json_file, "w+") as f:
-                json.dump(subtask_lengths, f)
+            if not os.path.isfile(stats_json_file):
+                subtask_lengths = dict()
+                if not os.path.isfile(demo_hdf5_file): continue
+                print(demo_hdf5_file)
+                with h5py.File(demo_hdf5_file, "r") as hdf5_f:
+                    for demo_key in hdf5_f["data"]:
+                        subtask_lengths[demo_key] = hdf5_f["data"][demo_key]["subtask_lengths"][:].tolist()
+                print("num of demos stats collected", len(subtask_lengths))
+                with open(stats_json_file, "w+") as f:
+                    json.dump(subtask_lengths, f)
+
+            shutil.copyfile(stats_json_file, dst_stats_json_file)
+            print(f"copied: {dst_stats_json_file}")
+            shutil.copyfile(important_stats_json_file, dst_important_stats_json_file)
+            print(f"copied: {dst_important_stats_json_file}")
