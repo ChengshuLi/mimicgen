fb4be63912ba947f0e27aa26f929bd58800a1ed7
Chengshu Li <chengshu@stanford.edu>
2025-05-15 23:28:08 -0700

    fix file path

--- mimicgen/scripts_viz/diveristy_fig.py
+++ mimicgen/scripts_viz/diveristy_fig.py
@@ -20,10 +20,10 @@ from PIL import Image
 import os
 import matplotlib.pyplot as plt
 
-image_folder = "/scr/chengshu/Downloads/images"
-# image_folder = "/vision/u/chengshu/figure_images"
-data_folder = "/mnt/chengshu"
-# data_folder = "/vision/u/chengshu"
+# image_folder = "/scr/chengshu/Downloads/images"
+image_folder = "/vision/u/chengshu/figure_images"
+# data_folder = "/mnt/chengshu"
+data_folder = "/vision/u/chengshu"
 
 gm.DATASET_PATH = "/cvgl2/u/chengshu/OmniGibson/omnigibson/data/og_dataset"
 
